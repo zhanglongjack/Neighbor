19c25326dccb02e25efdde36e91137f1adb88fc4
zhangyinkai <543123909@qq.com>
2018-11-28 22:52:23 +0800

    图片上传

--- src/main/java/com/neighbor/common/websoket/handler/impl/ImageMessageHandler.java
+++ src/main/java/com/neighbor/common/websoket/handler/impl/ImageMessageHandler.java
@@ -1,22 +1,16 @@
 package com.neighbor.common.websoket.handler.impl;
 
-import java.io.IOException;
-
 import com.neighbor.common.util.FileUploadUtil;
-import com.neighbor.common.websoket.service.SocketMessageService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
-
-import com.alibaba.fastjson.JSON;
 import com.neighbor.common.util.ResponseResult;
 import com.neighbor.common.websoket.constants.WebSocketChatType;
 import com.neighbor.common.websoket.constants.WebSocketMsgType;
 import com.neighbor.common.websoket.handler.WebSocketMessageHandler;
 import com.neighbor.common.websoket.po.SocketMessage;
+import com.neighbor.common.websoket.service.SocketMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class ImageMessageHandler implements WebSocketMessageHandler {
@@ -36,14 +30,14 @@ public class ImageMessageHandler implements WebSocketMessageHandler {
 	@Override
 	public void successCallBack(SocketMessage msgInfo, WebSocketChatType chatType, WebSocketMsgType msgType) {
 		// TODO Auto-generated method stub
-		logger.info("成功,更新消息状态:{}",msgInfo);
+		logger.info("图片消息成功,更新消息状态:{}",msgInfo);
 		socketMessageService.updateByPrimaryKeySelective(msgInfo);
 	}
 
 	@Override
 	public void failedCallBack(SocketMessage msgInfo, WebSocketChatType chatType, WebSocketMsgType msgType) {
 		// TODO Auto-generated method stub
-		logger.info("消息推送失败,更新消息状态:{}",msgInfo);
+		logger.info("图片消息推送失败,更新消息状态:{}",msgInfo);
 		socketMessageService.updateByPrimaryKeySelective(msgInfo);
 	}
  
