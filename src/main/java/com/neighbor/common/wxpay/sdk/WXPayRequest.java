0c48aa97bf093759c8f5762391f3a86bda60facb
zhangyinkai <543123909@qq.com>
2018-12-26 14:05:18 +0800

    导入包错误

--- src/main/java/com/neighbor/common/wxpay/sdk/WXPayRequest.java
+++ src/main/java/com/neighbor/common/wxpay/sdk/WXPayRequest.java
@@ -24,7 +24,7 @@ import java.net.UnknownHostException;
 import java.security.KeyStore;
 import java.security.SecureRandom;
 
-import static com.github.wxpay.sdk.WXPayConstants.USER_AGENT;
+import static com.neighbor.common.wxpay.sdk.WXPayConstants.USER_AGENT;
 
 public class WXPayRequest {
     private WXPayConfig config;
