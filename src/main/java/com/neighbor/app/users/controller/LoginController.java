0a26a2b21b053da72006b23bb5410b3d6eb9fab1
zhanglongjack <364666387@qq.com>
2019-03-26 18:23:54 +0800

    验证码位数错误修复

--- src/main/java/com/neighbor/app/users/controller/LoginController.java
+++ src/main/java/com/neighbor/app/users/controller/LoginController.java
@@ -170,7 +170,7 @@ public class LoginController {
 	@RequestMapping(value = "/sendSMS.req", method = RequestMethod.POST)
 	@ResponseBody
 	public ResponseResult sendSMS(@Length(message = "手机长度最少11位", min = 11) @NotNull(message = "手机号不能为空") String phone) {
-		String code = null;// TencentSms.createVerifyCode();
+		String code = TencentSms.createVerifyCode();
 		logger.info("发送验证码:" + phone);
 		TencentSms.smsSend(code, phone);
 		return new ResponseResult();
